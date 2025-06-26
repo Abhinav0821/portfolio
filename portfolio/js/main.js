40c0d63b27bb8ededf870781f55f8760e8b2ccd1
AbhinavKashyap <162035178+Abhinav0821@users.noreply.github.com>
2025-06-26 12:44:57 +0530

    Delete portfolio directory

--- portfolio/js/main.js
+++
@@ -1,7 +0,0 @@
-const btn = document.getElementById('theme-toggle');
-btn.addEventListener('click', () => {
-  document.body.classList.toggle('dark');
-  btn.innerHTML = document.body.classList.contains('dark')
-    ? '<i class="fas fa-sun"></i>'
-    : '<i class="fas fa-moon"></i>';
-});
